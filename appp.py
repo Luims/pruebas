057fa12b992577646e27e9f6050040848b31cb9b
Luims <106635850+Luims@users.noreply.github.com>
2024-12-02 20:40:44 -0600

    Create appp.py

---
+++ appp.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import numpy as np
+from datetime import datetime, timedelta
+st.title('sexo')
