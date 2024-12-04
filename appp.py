63aeeed05343a743196b0451ce5421eb89893d19
JuanJCL <106635889+JuanJCL@users.noreply.github.com>
2024-12-04 00:15:35 -0600

    Update appp.py

--- appp.py
+++ appp.py
@@ -152,7 +152,7 @@ def calcular_drawdown(precios):
     Retorna los valores en decimales (no en porcentaje)
     """
     high_water_mark = precios.expanding().max()
-    drawdown = (precios - high_water_mark) / high_water_mark
+    drawdown = (high_water_mark-precios) / high_water_mark
     return drawdown, high_water_mark
 
 def graficar_drawdown_financiero(precios, titulo="Análisis de Drawdown"):
