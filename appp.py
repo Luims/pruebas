1a7a903c8cee4d3ccf17b1fbf561abd4c3aaa769
Luims <106635850+Luims@users.noreply.github.com>
2024-12-06 14:15:28 -0600

    Update appp.py

--- appp.py
+++ appp.py
@@ -1006,7 +1006,7 @@ elif selection == "Portafolios óptimos":
 elif selection == "Backtesting":
     st.markdown('<div style="color:violet; font-size:40px; font-weight:bold;">Backtesting Óptimos </div>', unsafe_allow_html=True)
     portafolios = ["Portafolio con mínima volatilidad", "Portafolio máximo sharpe ratio", "Portafolio mínima volatilidad con objetivo de rendimiento de 10%"]
-    
+    portafolio_seleccionado = st.selectbox("Selecciona un portafolio:", portafolios)
     
     
     if portafolio_seleccionado == "Portafolio con mínima volatilidad":
