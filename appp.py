9ec918c4699cf89cc30adab5a53bcced291cb9bb
JuanJCL <106635889+JuanJCL@users.noreply.github.com>
2024-12-05 19:41:13 -0600

    Update appp.py

--- appp.py
+++ appp.py
@@ -647,13 +647,13 @@ elif selection == "Estadística de Activos":
           st.subheader(f'     {round(e[7],2)}%')
           
         #st.text(estadisticas(df['IEF_rend']))
-        fig_hist_asset = crear_histograma_distribucion(df['IEF_rend'],  VaR(df['IEF_rend']), CVaR(df['IEF_rend']),  f'Distribución de Retornos - {'IEF'}'  )
-        st.plotly_chart(fig_hist_asset, use_container_width=True, key="hist_asset")
+      fig_hist_asset = crear_histograma_distribucion(df['IEF_rend'],  VaR(df['IEF_rend']), CVaR(df['IEF_rend']),  f'Distribución de Retornos - {'IEF'}'  )
+      st.plotly_chart(fig_hist_asset, use_container_width=True, key="hist_asset")
       
-        simbolo = 'IEF'
-        start_date = '2010-01-01'
-        end_date = datetime.now()
-        drawdown(simbolo, start_date,end_date)
+      simbolo = 'IEF'
+      start_date = '2010-01-01'
+      end_date = datetime.now()
+      drawdown(simbolo, start_date,end_date)
 
 
 
