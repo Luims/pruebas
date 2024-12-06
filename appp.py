10fa0dc986a33dc30b9e632e3c08c125a2ff139c
JuanJCL <106635889+JuanJCL@users.noreply.github.com>
2024-12-06 01:58:19 -0600

    Update appp.py

--- appp.py
+++ appp.py
@@ -607,15 +607,15 @@ if  selection == "Activos":
        st.markdown('- El IAU es un ETF que busca replicar el precio del oro, proporcionando una forma eficiente de invertir en este activo sin poseerlo físicamente.')
        st.markdown('- Su exposición es al precio del oro, utilizado como cobertura contra inflación o diversificación en carteras de inversión.')
        st.markdown('- No invierte en empresas, sino directamente en oro físico almacenado en bóvedas.' )
-      
-  table = '''
-   | Asset | Índice  | Moneda | Países | Duración | Beta | Riesgos | Costo - Expense Ratio | Rendimiento |
-   | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- | ----------- |
-   | IEF | ICE U.S. Treasury 7-10 Year Bond Index | USD | Exclusivamente en Estados Unidos | 8 años - Sensibilidad intermendia | Cercana a 0 frente a acciones | Tasa Interés Moderado| 0.15% |  Moderado |
-  '''
+  
+  table = ''''
+  | Asset | Índice  | Moneda | Países | Duración | Beta | Riesgos | Costo - Expense Ratio | Rendimiento |
+  | ------------- | ------------- | ------------- | ------------- | ------------- | ------------- | ------------- | ------------- | ------------- |
+  | IEF | ICE U.S. Treasury 7-10 Year Bond Index | USD | Exclusivamente en Estados Unidos | 8 años - Sensibilidad intermendia | Cercana a 0 frente a acciones | Tasa Interés Moderado| 0.15% |  Moderado |
+  ''''
   
    
-  st.markdown(tables)
+  st.markdown(table)
 
 # Estadística de Activos
 
